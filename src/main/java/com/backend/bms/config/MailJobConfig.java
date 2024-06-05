7201e8a04e7d68d4183d5a076418930f11858ec0
wvssm <neoramiregi@gmail.com>
2024-06-05 15:39:30 +0900

    fix: batch 병렬처리로 수정

--- src/main/java/com/backend/bms/config/MailJobConfig.java
+++ src/main/java/com/backend/bms/config/MailJobConfig.java
@@ -7,30 +7,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
 
 @Configuration
 @EnableBatchProcessing
-public class MailJobConfiguration {
-    private static final Logger logger = LoggerFactory.getLogger(MailJobConfiguration.class);
+@EnableAsync
+public class MailJobConfig {
+    private static final Logger logger = LoggerFactory.getLogger(MailJobConfig.class);
 
     @Autowired
     private DataSource dataSource;
@@ -48,18 +52,19 @@ public class MailJobConfiguration {
     @Bean
     public Step sendBulkMailStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
         return new StepBuilder("sendMailStep", jobRepository)
-                .<User, User>chunk(20, platformTransactionManager) // 청크 크기 20
-                .reader(mailItemReader()) // User 객체를 읽어옴
-                .writer(mailItemWriter(null, null)) // User 객체로 메일을 보내는 것 수행
+                .<User, User>chunk(500, platformTransactionManager) // 청크 크기 조정
+                .reader(mailItemReader())
+                .writer(mailItemWriter(null, null))
+                .taskExecutor(taskExecutor()) // 병렬 처리
                 .build();
     }
 
     @Bean
     public ItemReader<User> mailItemReader() {
-        return new JdbcCursorItemReaderBuilder<User>()
-                .name("JdbcCursorItemReader")
-                .fetchSize(20)
-                .sql("SELECT id, name, email FROM user")
+        return new JdbcPagingItemReaderBuilder<User>()
+                .name("JdbcPagingItemReader")
+                .dataSource(dataSource)
+                .fetchSize(500) // 페이지 크기 조정
                 .rowMapper(new RowMapper<User>() {
                     @Override
                     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -69,7 +74,9 @@ public class MailJobConfiguration {
                                 .build();
                     }
                 })
-                .dataSource(dataSource)
+                .selectClause("SELECT id, name, email")
+                .fromClause("FROM user")
+                .sortKeys(Collections.singletonMap("id", Order.ASCENDING)) // 페이징을 위한 정렬 키
                 .build();
     }
 
@@ -77,15 +84,27 @@ public class MailJobConfiguration {
     @StepScope
     public ItemWriter<User> mailItemWriter(@Value("#{jobParameters['mailSubject']}") String subject,
                                            @Value("#{jobParameters['mailMessage']}") String mailMessage) {
-        return new ItemWriter<User>() {
-            @Override
-            public void write(Chunk<? extends User> items) throws Exception {
-                String[] emails = items.getItems().stream().map(User::getEmail).toArray(String[]::new);
-                long startTime = System.currentTimeMillis();
-                gmailService.sendEmail(emails, subject, mailMessage);
-                long endTime = System.currentTimeMillis();
-                logger.info("Chunk sent in {} ms", (endTime - startTime));
-            }
+        return items -> {
+            items.getItems().parallelStream().forEach(user -> {
+                try {
+                    long startTime = System.currentTimeMillis();
+                    gmailService.sendEmail(new String[]{user.getEmail()}, subject, mailMessage);
+                    long endTime = System.currentTimeMillis();
+                    logger.info("Email to {} sent in {} ms", user.getEmail(), (endTime - startTime));
+                } catch (Exception e) {
+                    logger.error("Failed to send email to {}", user.getEmail(), e);
+                }
+            });
         };
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(20); // 코어 스레드 수
+        taskExecutor.setMaxPoolSize(40); // 최대 스레드 수
+        taskExecutor.setQueueCapacity(200); // 큐 용량
+        taskExecutor.afterPropertiesSet();
+        return taskExecutor;
     }
 }
